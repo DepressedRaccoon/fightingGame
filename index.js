b1afbc8a558f3910c769398147cfcdf4f8c371cb
SChafinIII <118332995+DepressedRaccoon@users.noreply.github.com>
2024-04-07 13:54:16 -0500

    enemyAttackBox offset

--- index.js
+++ index.js
@@ -11,18 +11,23 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 const gravity = 0.7
 
 class Sprite {
-    constructor({ position, velocity, color = 'red' }) {
+    constructor({ position, velocity, color = 'red', offset }) {
         this.position = position;
         this.velocity = velocity;
         this.width = 50; 
         this.height = 150;
         this.lastKey;
         this.attackBox = {
-            position: this.position,
+            position: {
+                x: this.position.x,
+                y: this.position.y
+            },
+            offset,
             width: 100,
             height: 50,
         };
         this.color = color;
+        this.isAttacking
     }
 
     draw() {
@@ -30,12 +35,16 @@ class Sprite {
         c.fillRect(this.position.x, this.position.y, this.width, this.height);
 
         //attack box
-        c.fillStyle = 'green';
-        c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
+        if (this.isAttacking) {
+            c.fillStyle = 'green';
+            c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height);
+    }
     }
 
     update() {
         this.draw()
+        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
+        this.attackBox.position.y = this.position.y
 
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
@@ -43,6 +52,13 @@ class Sprite {
         if (this.position.y + this.height + this.velocity.y >= canvas.height) {
             this.velocity.y = 0
         } else   this.velocity.y += gravity
+    }
+
+    attack() {
+        this.isAttacking = true; 
+        setTimeout(() => {
+            this.isAttacking = false; 
+        }, 100)
     }
 }
 
@@ -54,6 +70,10 @@ const player = new Sprite({
     velocity: {
         x: 0,
         y: 10
+    },
+    offset: {
+        x: 0,
+        y: 0
     }
 });
 
@@ -64,6 +84,10 @@ const enemy = new Sprite({
     },
     velocity: {
         x: 0,
+        y: 0
+    },
+    offset: {
+        x: -50,
         y: 0
     },
     color: 'blue'
@@ -116,7 +140,8 @@ function animate() {
     }
 
     // detect for collision
-    if (player.attackBox.position.x + player.attackBox.width >= enemy.position.x && player.attackBox.position.x <= enemy.position.x + enemy.width && player.attackBox.position.y + player.attackBox.height >= enemy.position.y && player.attackBox.position.y <= enemy.position.y + enemy.height) {
+    if (player.attackBox.position.x + player.attackBox.width >= enemy.position.x && player.attackBox.position.x <= enemy.position.x + enemy.width && player.attackBox.position.y + player.attackBox.height >= enemy.position.y && player.attackBox.position.y <= enemy.position.y + enemy.height && player.isAttacking) {
+        player.isAttacking = false
         console.log('Beans')
     }
     }
@@ -136,7 +161,9 @@ window.addEventListener('keydown', (event) => {
         case 'w':
             player.velocity.y = -20
         break
-
+        case ' ':
+        player.attack()
+        break 
         case 'ArrowRight':
             keys.ArrowRight.pressed = true
             enemy.lastKey = "ArrowRight"
