eb6eace0a14588914d2bb7c0c10b76db6475c07c
SChafinIII <118332995+DepressedRaccoon@users.noreply.github.com>
2024-04-07 13:36:52 -0500

    Added basic collision/hitboxes

--- index.js
+++ index.js
@@ -11,16 +11,27 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 const gravity = 0.7
 
 class Sprite {
-    constructor({position, velocity}) {
+    constructor({ position, velocity, color = 'red' }) {
         this.position = position;
         this.velocity = velocity;
-        this.height = 150
-        this.lastKey
+        this.width = 50; 
+        this.height = 150;
+        this.lastKey;
+        this.attackBox = {
+            position: this.position,
+            width: 100,
+            height: 50,
+        };
+        this.color = color;
     }
 
     draw() {
-        c.fillStyle = 'red';
-        c.fillRect(this.position.x, this.position.y, 50, this.height);
+        c.fillStyle = this.color;
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+        //attack box
+        c.fillStyle = 'green';
+        c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
     }
 
     update() {
@@ -54,7 +65,8 @@ const enemy = new Sprite({
     velocity: {
         x: 0,
         y: 0
-    }
+    },
+    color: 'blue'
 });
 
 console.log(player);
@@ -102,7 +114,12 @@ function animate() {
     } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
         enemy.velocity.x = 5
     }
-}
+
+    // detect for collision
+    if (player.attackBox.position.x + player.attackBox.width >= enemy.position.x && player.attackBox.position.x <= enemy.position.x + enemy.width) {
+        console.log('Beans')
+    }
+    }
 
 animate()
 
