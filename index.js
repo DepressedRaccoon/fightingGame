b3dda0f54a5a34d6d43c750c8a81c5e74f686147
SChafinIII <118332995+SChafinIII@users.noreply.github.com>
2024-04-07 03:20:05 -0500

    Fix general errors

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
-canvas.width = 1024
-canvas.height = 576
+// Set canvas width and height
+canvas.width = 1024;
+canvas.height = 576;
 
+// Draw on the canvas
 c.fillRect(0, 0, canvas.width, canvas.height);
 
 class Sprite {
     constructor(position) {
-        this.position = position
+        this.position = position;
     }
 
     draw() {
-        c.fillSyle = 'red'
-        c.fillRect(this.position.x. this.position.y, 50, 150)
+        c.fillStyle = 'red';
+        c.fillRect(this.position.x, this.position.y, 50, 150);
     }
 }
 
 const player = new Sprite({
     x: 0,
     y: 0
-})
+});
 
 const enemy = new Sprite({
     x: 400,
     y: 100
+});
 
-})
+// Draw the sprites
+player.draw();
+enemy.draw();
 
-player.draw()
-enemy.draw()
-
-console.log(player); 
+console.log(player);
